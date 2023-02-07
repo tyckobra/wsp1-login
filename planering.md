4a2cd4281314625e6f0296f14da8c82191f355d2
Rasmus <rasmus.johansson2@elev.ga.ntig.se>
2023-02-07 14:59:59 +0100

    Halvt genom del 4

---
+++ planering.md
@@ -0,0 +1,37 @@
+# Databas
+
+## EFusers
+
+Username
+
+Password (Hash)
+
+# Views
+
+index
+
+login
+
+register
+
+profile
+
+# Routes
+
+get login       [Hämta allt ur EFusers + login.njk]
+
+post login      [Skickar Username och Password]
+
+get register    [Hämta allt ur EFusers + register.njk]
+
+post register   [Skickar Username och Password]
+
+## Om inloggad
+
+post logout     []
+
+get profile     [Hämta Username från EFusers + profile.njk]
+
+# Kod
+
+Standard
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index.njk', { title: 'Express' });
+    res.render('index.njk', 
+    { 
+        title: 'Login ALC'
+    });
 });
+
+router.get('/login', async function(req, res, next) {
+    res.render('login.njk',
+    {
+        title: 'Login'
+    });
+});
+
+router.post('/login', async function(req, res, next) {
+    const { username, password } = req.body;
+})
 
 module.exports = router;
