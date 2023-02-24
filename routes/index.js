c64ba8cff7d39bde6245f687b8cee376f1fd4b00
Rasmus <rasmus.johansson2@elev.ga.ntig.se>
2023-02-24 11:04:06 +0100

    hej hej - Albert 2023

--- routes/index.js
+++ routes/index.js
@@ -35,8 +35,7 @@ router.post('/login', async function (req, res, next) {
     else {
         const [user] = await promisePool.query(`SELECT * FROM efusers WHERE name = ?`, [username])
         bcrypt.compare(password, user[0].password, function (err, result) {
-            if(result === true)
-            {
+            if (result === true) {
                 req.session.user = user[0]  //Ifall det går att logga in, spara användarens data i sessions variabeln 'user'
                 return res.redirect('/profile')
 
@@ -45,25 +44,22 @@ router.post('/login', async function (req, res, next) {
                 return res.send('Invalid username or password')
             }
         })
-    } 
+    }
 });
 
-router.get('/profile', async function (req, res, next)
-{
-    if (req.session.user ) {        //Kollar ifall det finns en 'user' i sessionen
+router.get('/profile', async function (req, res, next) {
+    if (req.session.user) {        //Kollar ifall det finns en 'user' i sessionen
         res.render('profile.njk', {
             name: req.session.user.name
         })
     }
-    else{
+    else {
         return res.status(401).send('Access denied')
     }
 })
 
-router.post('/logout', async function (req, res, next)
-{
-    if(req.session.user)
-    {
+router.post('/logout', async function (req, res, next) {
+    if (req.session.user) {
         req.session.destroy()
         return res.redirect('/')
     }
@@ -72,14 +68,57 @@ router.post('/logout', async function (req, res, next)
     }
 })
 
-router.get('/crypt/:password', async function (req, res, next){
+router.get('/register', async function (req, res, next) {
+    res.render('register.njk')
+
+})
+
+router.post('/register', async function (req, res, next) {
+    const { username, password, passwordConfirmation } = req.body;
+    if (username === "" && password === "" && passwordConfirmation === "") {
+        return res.send('Username is Required')
+    }
+    else if (username === "") {
+        return res.send('Username is Required')
+    }
+    else if (password === "") {
+        return res.send('Password is Required')
+    }
+    else if (passwordConfirmation === "") {
+        return res.send('Passwords should match')
+    }
+
+    if (password == passwordConfirmation) {
+
+        bcrypt.hash(password, 10, async function (err, hash) {
+            console.log(hash)
+            const [rows] = await promisePool.query("SELECT * FROM efusers WHERE name = ?", [username])
+            console.log(rows[0])
+            if (rows.length === 0) {
+                const [user] = await promisePool.query("INSERT INTO efusers (name, password) VALUES (?, ?)", [username, hash])
+                req.session.user = user[0]
+                return res.redirect('/login')
+            }
+            else {
+                return res.send('Username is already taken')
+            }
+            //const [rows] = await promisePool.query("INSERT INTO efusers (name, password) VALUES (?, ?)", [username, hash])
+        });
+
+    }
+    else {
+        return res.send('Passwords do not match')
+    }
+})
+
+router.get('/crypt/:password', async function (req, res, next) {
     console.log(req.params)
     const password = req.params.password
 
     bcrypt.hash(password, 10, function (err, hash) {
         console.log(hash)
         return res.json({ hash });
-        
+
     });
 
 })
